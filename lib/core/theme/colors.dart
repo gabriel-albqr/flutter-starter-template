2704e8c5b142f05c8aa2447e72383c45c1f6ed3e
GabrielAlbq20 <gabriel.albqdev@outlook.com>
2025-07-14 22:42:48 -0300

    ✨ feat: adicionar utilitário validador de e-mail simples e adicionar cores de aplicativo centralizadas

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Colors.blue;
+  static const secondary = Colors.blueAccent;
+  static const background = Colors.white;
+  static const textPrimary = Colors.black87;
+}
---
+++ lib/shared/utils/validators.dart
@@ -0,0 +1,6 @@
+class Validators {
+  static bool isEmail(String value) {
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    return emailRegex.hasMatch(value);
+  }
+}
